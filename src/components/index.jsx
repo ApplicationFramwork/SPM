667b35f27deedd5aa8b733864c7717c158855f4c
vihangamalshan1998 <68722779+vihangamalshan1998@users.noreply.github.com>
2021-08-10 18:15:30 +0530

    edit

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import sideNavBarComponent from './sideNavBarComponent'
 
 export default class index extends Component {
     render() {
         return (
-            <div>
-               <h5>hello</h5> 
-            </div>
+            <body>
+                <sideNavBarComponent />
+                <div>
+                    <h5 className="head">hello</h5>
+                </div>
+            </body>
+            
         )
     }
 }
