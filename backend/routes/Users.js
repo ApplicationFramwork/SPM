18adfa9c564741a09204199a095693ecf809ec7b
Maleesha <malisha2019@gmail.com>
2021-09-04 23:19:54 +0530

    JWT added for backend

--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -4,42 +4,39 @@ const Teacher = require('../models/Teacher');
 const Student = require('../models/students');
 const JWT_SECRET = 'dseiow985344he02-238hfsdy22@@@sdtjerltmdjdguot';
 router.route("/login").post(async (req,res)=>{
-   const {username,password,type} = req.body;
+   const {Username,password,type} = req.body;
    let user;
-   let token;
 
    if(type=="Teacher"){
-      user = await Teacher.findOne({teacher_ID:username,NIC:password}).lean();
-      if(user){
-          token = jwt.sign({
-                id : user._id,
-              Name: user.firstName + user.lastName,
-              email : user.email,
-              type:'Teacher'
-          },JWT_SECRET
-          )
-      }else{
+       console.log(Username + " " + password)
+      user = await Teacher.findOne({teacher_ID:Username,NIC:password}).lean();
+      if(!user){
           return res.json({status : 'error', error: 'Invalid username/password'});
+
+      }else{
+          let token = jwt.sign({
+                  id : user._id,
+                  Name: user.firstName + user.lastName,
+                  email : user.email,
+                  type:'Teacher'
+              },JWT_SECRET
+          )
+          return res.json({status : 'ok' , token : token})
       }
    }else{
-       user = await Student.findOne({admissionNumber:username,firstName:password}).lean();
+       user = await Student.findOne({admissionNumber:Username,firstName:password}).lean();
        if(user){
-           token = jwt.sign({
+           let token = jwt.sign({
                    id : user._id,
                    Name: user.firstName + " " + user.lastName,
                    email : user.email,
                    type:'Student'
                },JWT_SECRET
            )
+           return res.json({status : 'ok' , token : token})
        }else{
            return res.json({status : 'error', error: 'Invalid username/password'});
        }
    }
-
-   if(token){
-       return res.json({status : 'ok' , token : token})
-   }
-
-
 });
 module.exports = router;
