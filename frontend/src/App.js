1f0c71098cf18332e41436fee2aea0b6a63a5e5e
Maleesha <malisha2019@gmail.com>
2021-09-06 01:08:59 +0530

    Index page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,15 @@ import AttendanceMark from "./components/AttendanceMark";
 import AddMarks from "./components/AddMarks";
 import ViewAttendance from "./components/ViewAttendance";
 import Login from "./components/login";
+import Index from "./components";
 
 function App() {
   return (
       <Router>
 
         <Switch>
-            <Route path="/" exact component={Login} />
+            <Route path="/" exact component={Index} />
+            {/*<Route path="/" exact component={Login} />*/}
             <Route path="/dashboard" exact component={AdminDashboard} />
             <Route path="/MarkAttendance" exact component={AttendanceMark} />
             <Route path="/AddMarks" exact component={AddMarks} />
---
+++ frontend/src/components/index.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Index extends Component {
+    render() {
+        return (
+            <div className={"indexBody"}>
+                <div className={"row"}>
+                    <div className="col-sm-5 m-5">
+                        <h1 className={"topic"}>Kidz School Management System</h1>
+                        <div className=" m-3 col-sm-3 getStartBtn">
+                            <h6>Get Started</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Index;
